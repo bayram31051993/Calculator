550f3ba61a87488e8dc866a9c8bfda3d56d815dc
User <yourgitemail@example.com>
2023-04-29 17:23:18 +0500

    Fix error

--- src/App.java
+++ src/App.java
@@ -3,34 +3,38 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("введите первое значение");
-            int a = scanner.nextInt();
-            if(a == 0) {
+            int a = Integer.parseInt(scanner.next());
+            if (a == 0) {
                 System.out.println("Значение должно быть больше 0");
                 return;
             }
-            System.out.println("введите второе значение");
-            int b = scanner.nextInt();
-            System.out.println("введите арифметические операции");
             char operation = scanner.next().charAt(0);
+            int b = Integer.parseInt(scanner.next());
+
             switch (operation) {
                 case '+':
-                    System.out.println(a + b);
+                    System.out.println("вывод: " + a + b);
                     break;
                 case '-':
-                    System.out.println(a - b);
+                    System.out.println("вывод: " + (a - b));
                     break;
                 case '*':
-                    System.out.println(a * b);
+                    System.out.println("вывод: " + a * b);
                     break;
                 case '/':
-                    System.out.println(a + b);
+                    if (b == 0) {
+                        System.out.println("Делимое не должно быть 0");
+                        return;
+                    }
+                    System.out.println("вывод: " + a / b);
+
                     break;
                 default:
                     System.out.println("Введенные данные не ссостветсвует к требованиес");
             }
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e
+                    + ": т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
 
     }
